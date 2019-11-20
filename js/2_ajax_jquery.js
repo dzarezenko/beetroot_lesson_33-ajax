618b78342be3243d820902e5d4ad7ad02a250635
Dmytro Zarezenko <dmytro.zarezenko@gmail.com>
2019-11-20 20:00:55 +0200

    jQuery $.get()

---
+++ js/2_ajax_jquery.js
@@ -0,0 +1,8 @@
+const API_URL = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+
+$(() => {
+  $.get(API_URL, (data) => {
+    console.log(data);
+    document.write("<pre>" + JSON.stringify(data, null, 2) + "</pre>");
+  });
+});
